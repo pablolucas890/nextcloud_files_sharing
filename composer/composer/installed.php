439db284dfb5c26d62bbb56f048e7b598a39c03b
pablolucas890 <pablolucas890@gmail.com>
2022-09-05 10:22:45 -0300

    first commit

---
+++ composer/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7669331be09dd3cb848182ae8edc2408802a4fb4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7669331be09dd3cb848182ae8edc2408802a4fb4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
